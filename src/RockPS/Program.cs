cfd3667390e3aa8c37bfa525afd1e35220e0e51f
Walls <caleb.walls@cgi.com>
2022-11-11 10:53:57 -0600

    first commit

---
+++ src/RockPS/Program.cs
@@ -0,0 +1,78 @@
+﻿/*Create a rock paper scissor games that takes user input
+  and does then based on the input prints out a random RPC
+  and then chooses a winnder based on results.*/
+using System;
+using System.Collections.Generic;
+
+namespace RockPS
+{
+
+
+
+
+
+
+
+class Program
+{
+
+   
+
+static void Main(string[] args)
+{
+    string userWeapon = "";
+    while(userWeapon != "invalid"){
+    //First I want the user to choose a weapon
+    Console.WriteLine("User Choose Your Weapon: 1.Rock, 2.Paper, or 3.Scissors");
+
+    //Get the users input
+    var tempchoice = Console.ReadLine();
+    //Need input validation later a throw and catch statement
+    /*
+    ............
+    */
+    var choice = int.Parse(tempchoice);
+    if((choice > 3 || choice < 1) )
+    {
+        break;
+    }
+
+//create a weapon object
+ var Weapobj = new Weapon();
+
+
+
+//Determine what the user choice is and store it
+ userWeapon = Weapobj.DetermineWeapon(choice);
+Console.WriteLine($"Computer Chose:{userWeapon}");
+
+//Then I want the computer to choose a weapon
+Random rmd = new Random();
+var computerChoice = rmd.Next(1,4);
+var computerWeapon = Weapobj.DetermineWeapon(computerChoice);
+Console.WriteLine($"Computer Chose:{computerWeapon}");
+
+    
+
+    //Then I want the program to pick a winner
+  Weapobj.PickWinnder(computerWeapon,userWeapon);
+
+   
+    }
+
+
+
+
+
+
+
+
+}
+
+
+}
+
+
+
+}
+
---
+++ src/RockPS/Weapons.cs
@@ -0,0 +1,130 @@
+
+namespace RockPS
+{
+
+
+
+
+
+
+    public class Weapon
+    {
+        //This is where weapon is defined
+        public string DetermineWeapon(int choice)
+        {
+            //Initalized my weapons in an array
+            string[] weap = {"Rock", "Paper", "Scissors"};
+            string result = "";
+
+            switch(choice)
+        {
+            case 1:
+            //I want this to be rock
+            result = weap[0];
+            break;
+
+            case 2:
+            //Paper
+            result = weap[1];
+            break;
+
+            case 3:
+            //Scissors
+            result = weap[2];
+            break;
+
+            default:
+            throw new ArgumentException("Invalid");
+            
+
+
+
+
+        }
+
+
+return result;
+
+
+
+        }
+
+
+
+
+
+
+
+public void PickWinnder(string computerWeap, string userWeap)
+{
+
+//Ties
+if(computerWeap == "Rock" && userWeap == "Rock")
+{
+    Console.WriteLine("DRAW");
+}
+if(computerWeap == "Scissors" && userWeap == "Scissors")
+{
+    Console.WriteLine("DRAW");
+}
+if(computerWeap == "Paper" && userWeap == "Paper")
+{
+    Console.WriteLine("DRAW");
+}
+
+
+
+//Computer Wins
+if(computerWeap == "Rock" && userWeap == "Scissors")
+{
+    Console.WriteLine("Computer WIN!!");
+}
+if(computerWeap == "Scissors" && userWeap == "Paper")
+{
+    Console.WriteLine("Computer WIN!!");
+}
+if(computerWeap == "Paper" && userWeap == "Rock")
+{
+    Console.WriteLine("Computer WIN!!");
+}
+
+
+//User Wins
+if(computerWeap == "Paper" && userWeap == "Scissors")
+{
+    Console.WriteLine("YOU WIN!!");
+}
+if(computerWeap == "Rock" && userWeap == "Paper")
+{
+    Console.WriteLine("YOU WIN");
+}
+if(computerWeap == "Scissors" && userWeap == "Rock")
+{
+    Console.WriteLine("YOU WIN");
+}
+
+
+
+}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
